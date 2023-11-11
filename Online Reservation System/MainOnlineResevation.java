f7302f44612b05dd5a589c5d6bd82d6e74c2086d
Prasad Pawar <106329241+PrasadPawarZ@users.noreply.github.com>
2023-11-12 00:22:11 +0530

    Add files via upload

---
+++ Online Reservation System/MainOnlineResevation.java
@@ -0,0 +1,8 @@
+package OnlineReservationSystem;
+
+public class MainOnlineResevation {
+    public static void main(String[] args) {
+        VisualForm form = new VisualForm();
+        form.createLoginPage();
+    }
+}
